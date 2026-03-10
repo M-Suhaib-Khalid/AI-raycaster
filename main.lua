cbe4fe2722d9548252f49f1aea70ca92e7d3eae8
Suhaib <mslayerwolf007@gmail.com>
2026-03-10 16:09:06 +0530

    texture system implemented

--- main.lua
+++ main.lua
@@ -1,9 +1,11 @@
 local map = require("map")
 
+
+-- player stuff
 local player = {
-    x = 2,
-    y = 14,
-    angle = 0,
+    x = 1.5,
+    y = 3,
+    angle = 1.5,
     width = 0.5,
     height = 0.5,
 }
@@ -22,6 +24,12 @@ function player:collision(x,y,w,h)
 
 end
 
+
+
+
+
+-- collision detector
+
 local function checkWallCollision()
 
     local tileX = math.floor(player.x)
@@ -30,7 +38,7 @@ local function checkWallCollision()
     for y = tileY - 1, tileY + 1 do
         for x = tileX - 1, tileX + 1 do
 
-            if map.grid[y+1] and map.grid[y+1][x+1] == 1 then
+            if map.grid[y+1] and map.grid[y+1][x+1] > 0 then
 
                 local wallX = x
                 local wallY = y
@@ -46,43 +54,77 @@ local function checkWallCollision()
     return false
 end
 
-local FOV = math.rad(45)
-local numRays = 512
+
+-- constants
+local FOV = math.rad(60)
+local numRays = 1200
 local maxDepth = 20
-local moveSpeed = 3
+local moveSpeed = 2
 local rotSpeed = 2
 local mouseSensitivity = 0.003
 
+
+
+
+-- load function
 function love.load()
+    love.graphics.setDefaultFilter("nearest", "nearest")
     love.mouse.setRelativeMode(true)
     player:updateRect()
+    love.window.setMode(1200,690)
+    textures = {
+    [1] = love.graphics.newImage("wall.png"),
+    [2] = love.graphics.newImage("door.png")
+    }
 end
 
+
+
+
+-- mouse movement
 function love.mousemoved(x, y, dx, dy)
     player.angle = player.angle + dx * mouseSensitivity
 end
 
+
+
+
+
+-- update function
 function love.update(dt)
+    -- strafing variables
+    local strafeX = math.cos(player.angle + math.pi/2)* dt
+    local strafeY = math.sin(player.angle + math.pi/2)* dt
     
-    local strafeX = math.cos(player.angle + math.pi/2) * moveSpeed * dt
-    local strafeY = math.sin(player.angle + math.pi/2) * moveSpeed * dt
+    -- tile location variables
     local tileX = math.floor(player.x / 1) + 1
     local tileY = math.floor(player.y / 1) + 1
     local tilePixelX = (tileX - 1) *1
     local tilePixelY = (tileY - 1) *1
     
+
+
+    -- movement
+
+    -- strafe left
     if love.keyboard.isDown("a") then
+        -- pos updater
         player.x = player.x - strafeX
         player.y = player.y - strafeY
         player:updateRect()
+        
+        -- collison check
         if checkWallCollision() then
             player.x = player.x + strafeX
             player.y = player.y + strafeY
             player:updateRect()
         end
+    
     end
 
+    -- strafe right
     if love.keyboard.isDown("d") then
+        -- pos update
         player.x = player.x + strafeX
         player:updateRect()
 
@@ -104,10 +146,9 @@ function love.update(dt)
     local dy = math.sin(player.angle) * moveSpeed * dt
 
     if love.keyboard.isDown("w") then
-
+        -- pos update
         local newX = player.x + dx
         local newY = player.y + dy
-
         player.y = newY
         player:updateRect()
 
@@ -126,12 +167,14 @@ function love.update(dt)
     end
 
     if love.keyboard.isDown("s") then
+        -- pos update
         local newX = player.x - dx
         local newY = player.y - dy
 
         player.y = newY
         player:updateRect()
 
+        
         if checkWallCollision() then
             player.y = player.y + dy
             player:updateRect()
@@ -145,6 +188,7 @@ function love.update(dt)
             player:updateRect()
         end
     end
+
     player:updateRect()
 end
 
@@ -183,6 +227,7 @@ function castRay(angle)
 
     local hit = false
     local side
+    local wallType = 1
 
     while not hit do
 
@@ -196,8 +241,11 @@ function castRay(angle)
             side = 1
         end
 
-        if map.grid[mapY+1] and map.grid[mapY+1][mapX+1] == 1 then
+        local tile = map.grid[mapY+1] and map.grid[mapY+1][mapX+1]
+
+        if tile and tile > 0 then
             hit = true
+            wallType = tile
         end
 
     end
@@ -210,7 +258,17 @@ function castRay(angle)
         perpWallDist = (mapY - player.y + (1 - stepY) / 2) / rayDirY
     end
 
-    return perpWallDist, side
+    local wallX
+
+    if side == 0 then
+        wallX = player.y + perpWallDist * rayDirY
+    else
+        wallX = player.x + perpWallDist * rayDirX
+    end
+
+    wallX = wallX - math.floor(wallX)
+
+    return perpWallDist, side, wallX, wallType
 end
 
 function love.draw()
@@ -218,63 +276,45 @@ function love.draw()
     local width = love.graphics.getWidth()
     local height = love.graphics.getHeight()
 
+    
     for i = 1, numRays do
+        
+        local rayAngle = player.angle - FOV/2 + (i-1)/numRays * FOV
 
-        local rayAngle = player.angle - FOV/2 + FOV * (i/numRays)
-
-        local dist,side = castRay(rayAngle)
-
+        local dist, side, wallX, wallType= castRay(rayAngle)
+        
         dist = dist * math.cos(rayAngle - player.angle)
-
+        
         local wallHeight = height / dist
-
-        local x = (i/numRays) * width
-
-        if side == 1 then
-            love.graphics.setColor(0.9, 0.9, 0.9) -- horizontal wall
-        else
-            love.graphics.setColor(1, 1, 1) -- vertical wall
-        end
-
-        love.graphics.rectangle(
-            "fill",
-            x,
-            height/2 - wallHeight/2,
-            width/numRays + 1,
-            wallHeight
+        local columnwidth = width / numRays
+        local x = (i-1) * columnwidth
+        
+        local tex = textures[wallType]
+        local texWidth = tex:getWidth()
+        local texHeight = tex:getHeight()
+        local texX = math.floor(wallX * texWidth)
+        
+        local quad = love.graphics.newQuad(
+            texX,
+            0,
+            1,
+            texHeight,
+            texWidth,
+            texHeight
         )
 
+        love.graphics.draw(
+            tex,
+            quad,
+            x,
+            height/2 - wallHeight/2,
+            0,
+            1,
+            wallHeight / texHeight
+        )
 
     end
-    love.graphics.setColor(0.5, 0.5, 1) 
-    love.graphics.rectangle("line",width/2-10,height/2-10,20,20)
-    love.graphics.circle("line",width/2,height/2,5)
-    local miniscale = 10
-    for y = 1, #map.grid do
-        for x = 1, #map.grid[y] do
 
-            if map.grid[y][x] == 1 then
-                love.graphics.setColor(1,1,1)
-            else
-                love.graphics.setColor(0.2,0.2,0.2)
-            end
-
-            love.graphics.rectangle(
-                "fill",
-                (x-1)*miniscale,
-                (y-1)*miniscale,
-                miniscale,
-                miniscale
-            )
-        end
-    end
-    love.graphics.setColor(0.8,0.1,0.1)
-    love.graphics.rectangle(
-    "fill",
-    player.rectX*miniscale,
-    player.rectY*miniscale,
-    player.width*miniscale,
-    player.height*miniscale)
 end
 
 function love.keypressed(key)
